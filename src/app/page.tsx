a7de864545e87d3b7cd63579e9381df4cda8f819
prabish01 <prabishdangi2002@gmail.com>
2026-06-14 14:00:45 +0545

    completion time 8 min added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ const features = [
 
 const trustItems = [
   {
-    text: "Takes around 5 minutes to complete",
+    text: "Takes around 8 minutes to complete",
     icon: <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />,
   },
   {
